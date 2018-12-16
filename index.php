f51d25f7649f1dd82274a5a30537b2b5f4dd22e0
danielb <daniel.berkovich@finantick.com>
2018-12-16 10:44:38 +0200

    Upload new code

--- index.php
+++ index.php
@@ -6,12 +6,12 @@ $categories = ["Total Task","Task Completed","Task Remaining"];
     <head>
         <title>Task</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" type="text/css" href="/style.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
         <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
         <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-        <script src="script.js"></script>
+        <script src="/script.js"></script>
     </head>
     <body>
         <div class="container bodyContainer" >
